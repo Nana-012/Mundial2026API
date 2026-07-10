29415d6a7a8e758dcb7b16d53e6a13d3ed8ff613
Nana-012 <gema._moreno@hotmail.com>
2026-07-10 09:37:51 +0200

    Forzando error

--- Mundial2026API/Controllers/SeleccionesController.cs
+++ Mundial2026API/Controllers/SeleccionesController.cs
@@ -28,7 +28,7 @@ public class SeleccionesController : ControllerBase
         var seleccion = await _context.Selecciones.FindAsync(id);
 
         if (seleccion == null)
-            return NotFound(new { mensaje = $"Selección con ID {id} no encontrada." });
+            return NotFound();
 
         return seleccion;
     }
